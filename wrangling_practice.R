81583c06feaab887e5222099d03f5b2584352c3a
jaslynmiura <jaslyntatsuko@gmail.com>
2025-08-19 12:30:06 -0700

    dates and times

--- wrangling_practice.R
+++ wrangling_practice.R
@@ -75,3 +75,37 @@ animals %>%
   filter(!location %in% sites$location)
 
 # answer would change if we changed the order of the data frames
+
+## Practice with Lubridate
+my_date <- "03-13-1998"
+lubridate::mdy(my_date) # fixed date to ISO 8601
+
+# new format for date
+my_date <- "08-Jun-1974"
+lubridate::dmy(my_date)
+
+# another example of a different format
+my_date <- "19160518"
+lubridate::ymd(my_date)
+
+# what happens if we give lubridate a date that doesn't make sense?
+lubridate::mdy("1942-08-30")
+
+lubridate::dmy("09/12/84")
+
+# working with date-times
+
+time <- "2020-08-12 11:18"
+time <- ymd_hm(time)
+
+# convert to PDT
+with_tz(time, "America/Los_Angeles")
+
+# extract info from dates
+week(time)
+year(time)
+day(time)
+
+start_time <- Sys.time()
+end_time <- Sys.time()
+end_time - start_time
